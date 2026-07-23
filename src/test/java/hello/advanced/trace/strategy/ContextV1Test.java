dce034eb8348e35c35494d7720e74140db0dc195
yang <soochang.yang@gmail.com>
2026-07-23 18:36:23 +0900

    Spring advanced design pattern template, strategy init #2

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -2,6 +2,7 @@ package hello.advanced.trace.strategy;
 
 import hello.advanced.trace.strategy.code.strategy.ContextV1;
 import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -34,11 +35,11 @@ public class ContextV1Test {
     @Test
     void strategyV1(){
         StrategyLogic1 strategyLogic1 = new StrategyLogic1();
-        ContextV1 contextV1 = new ContextV1(strategyLogic1);
-        contextV1.execute();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
 
-        StrategyLogic1 strategyLogic2 = new StrategyLogic1();
-        ContextV1 contextV2 = new ContextV1(strategyLogic1);
-        contextV2.execute();
+        StrategyLogic2 strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
     }
 }
